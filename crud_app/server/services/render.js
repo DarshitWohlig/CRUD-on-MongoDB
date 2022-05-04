113ee2c8e2e62af852ca366e21a774d63783aa01
Darshit Thakker <darshitinternship@gmail.com>
2022-05-04 12:13:12 +0530

    AAAA 12:12  04/05/22

--- crud_app/server/services/render.js
+++ crud_app/server/services/render.js
@@ -2,17 +2,31 @@ const axios=require('axios'); //Allows to make a req
 
 
 exports.homeRoutes=function(req,res){
-  
+    
+    try{
+       /* let {page,size}= req.query;
+        if(!page){
+            page=1;
+        }
+        if(!size){
+            size=5;
+        }
+
+        const limit=parseInt(size);
+        const skip = (page-1)*size; */
+
     //make req to api/users
     axios.get('http://localhost:5000/api/users')
     .then(function(response){
-        console.log(response.data);
+        //console.log(response.data);
         res.render('index.ejs',{users:response.data});
     }) 
-    .catch(function(err){
-        res.send(err)
-    });
 }
+    catch(err){
+    res.send(err)
+    };
+}
+
 
 exports.add_user=function(req,res){
     res.render('add_user')
